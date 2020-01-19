32641d1ce858b8550280aec46fb7814e239b606e
Ziomek <kamsebgoralski@gmail.com>
2020-01-19 17:56:53 +0100

    Changed package name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "simplealgorithms",
+            name: "SimpleAlgorithms",
             targets: ["simplealgorithms"]),
     ],
     dependencies: [
